0c15a6a4c280934b687d81dad1cb56ab3a8f400d
Marco Amato <amatomarco111@gmail.com>
2022-06-25 20:42:52 +0200

    insert sentiment done

--- src/MySqlQueries.py
+++ src/MySqlQueries.py
@@ -1,31 +1,47 @@
 import sys
+from typing import Tuple
 
 import mariadb
 
-already_connected_mariadb = False
 
+class DBConnection:
+    already_connected_mariadb = False
+    cursor = None
+    db_connection = None
 
-def connect_to_db():
-    cur = None
+    def connect_to_db(self):
+        # Connect to MariaDB Platform
+        if not self.already_connected_mariadb:
+            try:
+                self.db_connection = mariadb.connect(
+                    user="root",
+                    password="test",
+                    host="localhost",
+                    port=3306,
+                    database="progetto"
+                )
+            except mariadb.Error as e:
+                print(f"Error connecting to MariaDB Platform: {e}")
+                sys.exit(1)
 
-    # Connect to MariaDB Platform
-    if not already_connected_mariadb:
-        try:
-            conn = mariadb.connect(
-                user="root",
-                password="test",
-                host="localhost",
-                port=3306,
-                database="progetto"
-            )
-        except mariadb.Error as e:
-            print(f"Error connecting to MariaDB Platform: {e}")
-            sys.exit(1)
+            # Get Cursor
+            self.cursor = self.db_connection.cursor()
 
-        # Get Cursor
-        cur = conn.cursor()
+        self.cursor.execute("SHOW TABLES")
 
-    cur.execute("SHOW TABLES")
+        for (table_name,) in self.cursor:
+            print(table_name)
 
-    for (table_name,) in cur:
-        print(table_name)
+    def insert_sentiment(self, sentiment: str):
+        insert_query = "INSERT INTO sentiment VALUES ('{}');".format(sentiment)
+        self.launch_query(insert_query)
+
+    def delete_lex_res(self):
+        delete_query = "DELETE FROM lexicalresources"
+        self.launch_query(delete_query)
+        print(self.cursor.rowcount, "record(s) deleted")
+
+    def launch_query(self, query):
+        self.cursor.execute(query)
+        self.db_connection.commit()
+
--- test/test_sql.py
+++ test/test_sql.py
@@ -1,9 +1,21 @@
-from src.MySqlQueries import connect_to_db
+from src.MySqlQueries import DBConnection
 
 
 def test_db_connection():
-    connect_to_db()
+    db_connection = DBConnection()
+    db_connection.connect_to_db()
+    return db_connection
+
+
+def test_insert_sentiment():
+    connection = test_db_connection()
+    connection.insert_sentiment("Pinocchio")
+
+
+def test_delete_lex_res():
+    connection = test_db_connection()
+    connection.delete_lex_res()
 
 
 if __name__ == "__main__":
-    test_db_connection()
+    test_insert_sentiment()
